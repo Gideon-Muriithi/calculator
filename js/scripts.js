bf06a017b9e11b80633f78f59fcaf0e2cc8697db
Gideon-Muriithi <gideongakenge@gmail.com>
2019-07-18 10:53:48 +0300

    Add changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
-var add = function(num1, num2){
-  return num1 + num2;
-}
-var num1 = (parseInt("Enter a number:"));
-var num2 = (parseInt("Enter a number:"));
-alert(add(num1,num2));
+var add = function(number1, number2) {
+return number1 + number2;
+};
+var subtract = function(number1, number2) {
+return number1 - number2;
+};
+var multiply = function(number1, number2) {
+return number1 * number2;
+};
+var divide = function(number1, number2) {
+return number1 / number2;
+};
+
+$(document).ready(function() {
+$("form#add").submit(function(event) {
+event.preventDefault();
+var number1 = parseInt($("#add1").val());
+var number2 = parseInt($("#add2").val());
+var result = add(number1, number2);
+$("#output").text(result);
+});
+});
